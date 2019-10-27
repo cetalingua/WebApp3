984dd54d31cb448ab3b713f9bda5cb3542ce8d81
cetalingua <56362704+cetalingua@users.noreply.github.com>
2019-10-27 15:41:50 -0400

    Update server.py

--- google-app-engine/app/server.py
+++ google-app-engine/app/server.py
@@ -26,9 +26,11 @@ async def download_file(url, dest):
 
 async def setup_learner():
     await download_file(model_file_url, path/'models'/f'{model_file_name}.pth')
-    data_bunch = ImageDataBunch.single_from_classes(path, classes,
-        ds_tfms=get_transforms(), size=(100,180).normalize(imagenet_stats)
-    learn = cnn_learner(data_bunch, models.resnet50, pretrained=False)
+    data_bunch = ImageDataBunch.single_from_classes(path, classes, size=(100,180)).normalize(imagenet_stats)
+    #data_bunch = ImageDataBunch.single_from_classes(path, classes,
+        #ds_tfms=get_transforms(), size=(100,180).normalize(imagenet_stats)
+    learn = create_cnn(data_bunch, models.resnet50, pretrained=False)
+    #learn = cnn_learner(data_bunch, models.resnet50, pretrained=False)
     learn.load(model_file_name)
     return learn
 
